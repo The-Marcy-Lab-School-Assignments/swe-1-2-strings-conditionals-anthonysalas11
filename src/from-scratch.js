4aa311bb382094a2e4a3125e26dd22d4284d71c0
anthonysalas11 <anthony.salas.ts@gmail.com>
2025-10-09 09:54:48 -0400

    started on assignment

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,7 +1,37 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    return `drought`
+  }
+  else if (inches < 2) {
+    return `dry`
+  } else if (inches < 4) {
+    return `average`
+  }
+  else if (inches < 6) {
+    return `rainy`
+  }
+  else {
+    return 'flood'
+  }
 };
 
-const happyBirthdayPet = () => {
+
+const happyBirthdayPet = (breed, age) => {
+  if (breed === "snake") {
+    return 'Hiss Hiss'
+  }
+  else if (breed === 'cat' && age < 5) {
+    return `Mew Mew!`
+  } else if (breed === 'cat' && age > 5) {
+    return 'Meow Meow'
+  }
+  else if (breed === dog && age < 5) {
+    return
+  }
+  else if (breed === 'dog' && age >= 5 && age <= 10) {
+    return 'Woof Woof'
+  }
+
 };
 
 const funTypes = () => {
