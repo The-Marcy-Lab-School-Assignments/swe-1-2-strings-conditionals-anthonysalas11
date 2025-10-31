7231bc03f606a7fc9b67fddf44e0680be369764f
anthonysalas11 <anthony.salas.ts@gmail.com>
2025-10-31 13:36:11 -0400

    progress made

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -18,12 +18,12 @@ const measureRain = (inches) => {
 
 const happyBirthdayPet = (breed, age) => {
   if (breed === "snake") {
-    return 'Hiss Hiss'
+    return 'Hiss hiss!'
   }
   else if (breed === 'cat' && age < 5) {
-    return `Mew Mew!`
+    return `Mew mew!`
   } else if (breed === 'cat' && age > 5) {
-    return 'Meow Meow'
+    return 'Meow meow'
   }
   else if (breed === dog && age < 5) {
     return
@@ -41,16 +41,44 @@ const funTypes = (jsType) => {
 const rounder = () => {
 };
 
-const formatName = () => {
+const formatName = (first, last) => {
+
+  console.log(first, last)
+
+  let newFirst = first.slice(1).toLowerCase()
+  let newLast = last.slice(1).toLowerCase()
+
+  let firstNameStart = first[0].toUpperCase()
+  let lastNameStart = last[0].toUpperCase()
+
+  return `${firstNameStart + newFirst} ${lastNameStart + newLast}`
+
+};
+//console.log(formatName('MARY', 'JUNE'))
+const extractDomain = (email) => {
+  return email.split('@')[1]
+
+};
+console.log(extractDomain)
+
+
+
+
+const startsWithVowel = (str) => {
+  let arr = ['a', 'e', 'i', 'o', 'u']
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === str[0].toLowerCase()) {
+      return true;
+    }
+
+  }
+  return false
 };
 
-const extractDomain = () => {
-};
-
-const startsWithVowel = () => {
-};
-
-const rotate = () => {
+const rotate = (str, num) => {
+  for (let i = 0; i < str.length; i++) {
+    if ()
+}
 };
 
 module.exports = {
